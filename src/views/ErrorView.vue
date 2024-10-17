68a74dc8bb87cba5c245a96b97a77833a0c590ea
casalaspro <casalaspro.alessandro@gmail.com>
2024-10-17 19:44:37 +0200

    REFACTORY - ErrorView.vue

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,25 +1,18 @@
 <template>
   <v-container>
+    
+    <!-- here there is a button to redirect the user on the login view -->
     <router-link class="myBtn d-flex justify-center mt-5" to="/">
       <v-btn color="white">Please, go to Login before to see the to-do-list. <span style='font-size:50px;'>&#129430;</span></v-btn>
     </router-link>
+
   </v-container>
-  
 </template>
 
 <script>
-export default{
-  name: "ErrorView",
-  data(){
-    return{
-
-    }
-  },
-  methods:{
-
+  export default{
+    name: "ErrorView"
   }
-}
-
 </script>
 
 <style lang="css">
