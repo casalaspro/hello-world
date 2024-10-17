d9763090dc8a6d465b9cfc2b7224ac284ab8412c
casalaspro <casalaspro.alessandro@gmail.com>
2024-10-17 19:40:36 +0200

    REFACTORY - LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,10 @@
 <template>
   <v-card width="400" class="mx-auto mt-5">
+
       <v-card-title class="pb-5">
         <h1>Login</h1>
       </v-card-title>
+
       <v-card-text>
         <v-form class="">
           <v-text-field 
@@ -16,21 +18,23 @@
           />
         </v-form>
       </v-card-text>
+
       <v-divider></v-divider>
+
       <v-card-actions>
         <v-spacer></v-spacer>
         <div @click="sendNameToApp()">
+          <!-- in this router link i pass parameters -->
           <router-link :to="{ name: 'todolist', params: { name: insertedName } }">
             <v-btn class="primary">Go to list</v-btn>
           </router-link>
         </div>
       </v-card-actions>
+
     </v-card>
 </template>
 
 <script>
-
-
   export default {
     name: 'LoginView',
     data(){
@@ -40,22 +44,16 @@
       }
     },
     methods:{
+      // i send the value in the input with the $emit method and i redirect to to-do-list or i send an error
       sendNameToApp(){
         if(this.insertedName !== ""){
           this.$emit('user', this.insertedName)
           this.$router.push({ name: 'todolist', params: { name: this.insertedName } });
         }else{
+          // i set the error message
           this.errorMessage = "Username can't be empty"
         }
-        
       },
-      // goToTodolist() {
-        
-      //   this.$router.push({ name: 'todolist' });
-      // }
-    },
-    components: {
-      
     },
   }
 </script>
